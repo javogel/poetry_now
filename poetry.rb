bb22dc3916269720bbd5723519b150041610243f
Johannus V <johannus.vogel@gmail.com>
2016-08-09 14:42:37 +0200

    you can now send string as parameter for app to build acrostic

--- poetry.rb
+++ poetry.rb
@@ -3,11 +3,19 @@ require 'httparty'
 # require 'sinatra/reloader'
 
 
-get '/hi' do
-
+get '/' do
 
   @poem = get_poetry
+  erb :home
 
+end
+
+
+get '/:id' do
+
+
+  @poem = get_poetry(params[:id])
+  @acrostic = params[:id]
   erb :home
 
   # @poem.to_s
@@ -17,17 +25,17 @@ end
 
 
 
-def get_poetry
+def get_poetry(acrostic = "volatoris")
     final_poem = []
-    anagram_array = "volatoris".split("")
+    acrostic_array = acrostic.split("")
     # poetry = HTTParty.get("http://poetrydb.org/author,linecount/Shakespeare;14/lines").to_a
     poetry = HTTParty.get("http://poetrydb.org/lines/love/author,lines,linecount").to_a
     # (0..13).each { |i| poem << poetry[rand(154)]['lines'][i] }
     poetry = poetry.shuffle
-    while final_poem.length < anagram_array.length
+    while final_poem.length < acrostic_array.length
       poetry.each do |poem|
         poem['lines'].each do |line|
-            condition = (line[0] != nil) && (line[0].downcase == anagram_array[final_poem.length])
+            condition = (line[0] != nil) && (line[0].downcase == acrostic_array[final_poem.length])
 
 
           if condition
