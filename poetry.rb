08694ec1d59973fa0f1f6589fd2f10ba2edbee78
Johannus V <johannus.vogel@gmail.com>
2016-08-09 13:53:55 +0200

    random acrostic working, and improved stying and fonts

--- poetry.rb
+++ poetry.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'httparty'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/hi' do
 
@@ -9,14 +10,37 @@ get '/hi' do
 
   erb :home
 
+  # @poem.to_s
+
 end
 
 
 
 
 def get_poetry
-    poem = []
-    poetry = HTTParty.get("http://poetrydb.org/author,linecount/Shakespeare;14/lines").to_a
-    (0..13).each { |i| poem << poetry[rand(154)]['lines'][i] }
-    poem
+    final_poem = []
+    anagram_array = "volatoris".split("")
+    # poetry = HTTParty.get("http://poetrydb.org/author,linecount/Shakespeare;14/lines").to_a
+    poetry = HTTParty.get("http://poetrydb.org/lines/love/author,lines,linecount").to_a
+    # (0..13).each { |i| poem << poetry[rand(154)]['lines'][i] }
+    poetry = poetry.shuffle
+    while final_poem.length < anagram_array.length
+      poetry.each do |poem|
+        poem['lines'].each do |line|
+            condition = (line[0] != nil) && (line[0].downcase == anagram_array[final_poem.length])
+
+
+          if condition
+            final_poem << line
+          end
+
+          # binding.pry
+
+          next if condition
+        end
+        poetry = poetry.shuffle
+      end
+    end
+    # (0..13).each { |i| poem << poetry[rand(154)]['lines'][i] }
+    final_poem
 end
